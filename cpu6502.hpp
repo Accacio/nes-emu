81129e69b43d798b68f838ba23d12667ee1939cd
Accacio <raccacio@poli.ufrj.br>
2020-06-07 20:03:33 +0200

    cpu 6502

---
+++ cpu6502.hpp
@@ -0,0 +1,15 @@
+
+#ifndef __CPU6502_H_
+#define __CPU6502_H_
+
+
+class cpu6502 {
+    public:
+        cpu6502();
+        ~cpu6502();
+
+
+    private:
+};
+
+#endif // __CPU6502_H_
--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 
 #include <iostream>
+#include "cpu6502.hpp"
 
 int main(void)
 {
+    cpu6502 cpu = cpu6502();
     return(0);
 }
